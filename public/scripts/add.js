1b8d1bb07f48a314f0a7afaa82c43374bd1fdb49
Qing <592423741@qq.com>
2017-05-17 17:39:41 +0800

    接口处理好了

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -6,5 +6,12 @@ define(function (require,exports,module) {
         //1、将数据提交到  接口
         //2、使用post
         //3、传递哪些参数
+        var formData = $(this).serialize();
+        $.ajax({
+            url:'/teacher/add',
+            type:'post',
+            data:formData ,
+        })
+        return false;//阻止默认行为
     });
 })
